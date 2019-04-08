d6c0cdb8ccea034cebc20cd74a32b5765a6add27
James Declerus <james.declerus@esih.edu>
2019-04-08 15:29:35 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,27 @@
 # code your #valid_move? method here
+  board = [" ", " ", "X", " ", " ", " ", " ", "O", " "]
+puts "Enter a position on the board 1-9:"
+index=STDIN.gets.chomp.to_i
+n_index=index - 1
+def valid_move?(board, index)
+if(position_taken?(board, n_index)==" ")
+  return true
+else
+  return nil
+end
 
+ if(board[n_index].between?(0, 8))
+   return true
+ else
+   return
+   nil
+ end
+end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, n_index)
+  if(board[n_index]=="X" || board[n_index]=="O")
+    return true
+  else
+    return false
+end
+end
